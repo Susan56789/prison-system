89a1aa07511cf585da500a17a04cf34c97796d4a
Susan Wairimu <mwihaki2susan@gmail.com>
2022-05-21 21:48:26 +0300

    prison location

---
+++ section.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="screen" href="login.css">
+    <title>Location</title>
+</head>
+
+<body>
+    <table align="center" border="0" bgcolor="white" width="400" cellpadding="9" cellspacing="0" height="525">
+        <tr>
+            <td colspan="2" height="2"><img src="banner.gif" width="860"></td>
+        </tr>
+        <tr>
+            <td colspan="3" bgcolor="#FF0000" height="1" align="center">
+                <font size="4">
+                    <a href="index.php">HOME</a> |
+                    <a href="login2.php">LOGIN </a> |
+                    <a href="announce.php">COMMENT</a>
+                </font>
+            </td>
+        </tr>
+        <tr>
+            <td width="4%" bgcolor="#FFFFFF" valign="top">
+                <h3 align="center" title="You should be online">
+                    &nbsp;
+                </h3>
+            </td>
+
+            <td width="11%" valign="top" bgcolor="#FFFFFF">
+                <br />
+                <div id="content">
+                    <div id="gallerycontainer">
+                        <div class="portfolio-area">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15955.90622136444!2d36.8946376!3d-1.1769666!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x11bc4ddfa59a7039!2sKamiti%20Maximum%20Security%20Prison!5e0!3m2!1sen!2ske!4v1653158420182!5m2!1sen!2ske" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            <br />
+
+                            <div class="column-clear"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
