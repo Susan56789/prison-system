e89f96b5a48d37e90dbf382cf2464e9b4eb726a0
Susan Wairimu <mwihaki2susan@gmail.com>
2022-05-26 18:11:10 +0300

    updates

--- admin/addAdmin.php
+++ admin/addAdmin.php
@@ -6,10 +6,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
     <link href="login.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" media="screen" href="login.css">
+    
 </head>
 
 <body>
