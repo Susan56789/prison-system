a8eae5694d687c6a4ce5d66e32c5aed9a5d75df4
Susan Wairimu <mwihaki2susan@gmail.com>
2022-06-14 12:32:40 +0300

    reset password

--- recovery.php
+++ recovery.php
@@ -17,15 +17,40 @@ if (strlen($password) < 6) {
 }
 
 if ($Usertype == 'Police') {
+    $sel = mysqli_query($con, "SELECT * from police_tbl WHERE UserName='$username'");
+
+    $row = mysqli_fetch_array($sel);
+    //check if username is already in database
+    if ($row['UserName'] !== $username) {
+        echo '<script type="text/javascript">alert("Username does not exist!");window.location=\'forgotPassword.php\';</script>';
+    }
+
     $sql =  "UPDATE  police_tbl SET Password='$password'
      WHERE UserName='$username' and Email='$email'";
 }
 
 if ($Usertype == 'User') {
+    $sel = mysqli_query($con, "SELECT * from user_tbl WHERE UserName='$username'");
+
+    $row = mysqli_fetch_array($sel);
+    //check if username is already in database
+    if ($row['UserName'] !== $username) {
+        echo '<script type="text/javascript">alert("Username does not exist!");window.location=\'forgotPassword.php\';</script>';
+    }
+
     $sql =  "UPDATE  user_tbl SET Password='$password'
     WHERE UserName='$username' and Email='$email'";
 }
 if ($Usertype == 'Admin') {
+    $sel = mysqli_query($con, "SELECT * from admin_tbl WHERE Usename='$username'");
+
+    $row = mysqli_fetch_array($sel);
+
+    //check if username is already in database
+    if ($row['Usename'] !== $username) {
+        echo '<script type="text/javascript">alert("Username does not exist!");window.location=\'forgotPassword.php\';</script>';
+    }
+
     $sql =  "UPDATE  Admin_Tbl SET Admin_Password='$password'
      WHERE Usename='$username' and Email='$email'";
 }
